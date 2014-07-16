0490d53536e1237a576dbabe8fae8447eaaef70b
hxtpoe <hxtpoe@gmail.com>
2014-07-16 21:56:41 +0200

    jshint merge

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,14 +3,6 @@ define([
     'angular',
     'controllers/main',
     'routeManager',
-<<<<<<< HEAD
-    'angular-ui-router']/*deps*/, function (angular, MainCtrl, RouteManager)/*invoke*/ {
-    'use strict';
-
-    console.log("scripts");
-
-    return angular.module('connectApp', [
-=======
     'angular-ui-router'
   ],
     /*deps*/
@@ -18,19 +10,12 @@ define([
         'use strict';
 
         return angular.module('connectApp', [
->>>>>>> angular-ui-route
             'connectApp.controllers.MainCtrl',
             /*angJSDeps*/
             'ngCookies',
             'ngResource',
             'ngSanitize',
             'ui.router'
-<<<<<<< HEAD
-        ])
-        .config(RouteManager);
-});
-=======
           ])
         .config(RouteManager);
       });
->>>>>>> angular-ui-route
